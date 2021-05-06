9e29195d50ef17fb0bc039db9dc55c3ea60ee3ac
Fizoratti <robson.felipe@acad.pucrs.br>
2021-05-05 23:45:47 -0300

    Finalizada a entrega 2

--- src/main/java/software/Escalonador.java
+++ src/main/java/software/Escalonador.java
@@ -2,50 +2,35 @@ package software;
 
 import virtualmachine.VM;
 
-import java.util.concurrent.Semaphore;
+import java.util.Queue;
 
 import util.Console;
 
 public class Escalonador extends Thread {
     private static Escalonador INSTANCE;
 
-    public static Semaphore useESC;
-    private volatile int cont;
+    private volatile int counter;
 
-    private Escalonador() {
-        useESC = new Semaphore(1);
-    }
+    private Escalonador() {}
 
-    //run da thread escalonador, espera alguem mandar ela executar, 
-    //apos alguem mandar executar, 1 processo por vez,
-    //espera que a rotina de tratamento libere o uso da cpu para voltar a escalonar
+
     public void run() {                     Console.debug(" > Escalonador.run()");
         while (true) {
-            try {
-//                VM.semESC.acquire();
-            } catch (Exception e) {
-            }
-            cont = 0;
-            try {
-                useESC.acquire();
-            } catch (Exception e) {
-            }
+            Queue<PCB> processes = VM.get().pm.pcbList;
+            counter = 0;
 
-            //apos liberado, fica testando se a fila é > 0, caso seja executa 1 vez e espera a liberacao
-            //e caso seja 0, continua esperando para escalonar
-            while (cont == 0) {
-                //pega o primeiro da fila de prontos, muda status para executando, seta contexto na cpu, retira da fila de prontos, libera cpu.
-                if (VM.get().pm.pcbList.size() > 0) {
-                    VM.get().pm.pcbList.peek().status = StatusPCB.EXECUTANDO;
+            while (counter == 0) {
+                if (processes.size() > 0) {
+                    processes.peek().status = Status.RUNNING;
                     VM.get().cpu.setContext(
-                        VM.get().pm.pcbList.peek().allocatedPages, 
-                        VM.get().pm.pcbList.peek().pc, 
-                        VM.get().pm.pcbList.peek().id, 
-                        VM.get().pm.pcbList.peek().reg
+                        processes.peek().allocatedPages, 
+                        processes.peek().pc, 
+                        processes.peek().id, 
+                        processes.peek().reg
                     );
-                    VM.get().pm.pcbList.remove(VM.get().pm.pcbList.peek());
+                    processes.remove(processes.peek());
                     VM.get().cpu.run();
-                    cont++;
+                    counter++;
                 }
             }
         }
--- src/main/java/software/PCB.java
+++ src/main/java/software/PCB.java
@@ -6,34 +6,24 @@ import java.util.ArrayList;
 
 public class PCB {
     public int id;
+    public Interrupt interrupt;
     public ArrayList<Integer> allocatedPages;
 
     // CPU context
     public int pc;
-    public StatusPCB status;
+    public Status status;
     public int[] reg;
-    public int nVezesCPU;
-    public Interrupt irpt;
-    public int valorEscritaLeitura;
-    public int posicaoEscritaLeitura;
-
-//    public PCB(int id, ArrayList<Integer> allocatedPages) {
-//        this.id = id;
-//        this.allocatedPages = allocatedPages;
-//    }
 
     public PCB(int id, ArrayList<Integer> allocatedPages) {
         this.allocatedPages = allocatedPages;
         this.id = id;
-        this.irpt = Interrupt.NONE;
+        this.interrupt = Interrupt.NONE;
         this.pc = 0;
-        this.status = StatusPCB.PRONTO;
-        this.nVezesCPU = 0;
-        this.valorEscritaLeitura = 0;
-        this.posicaoEscritaLeitura = 0;
+        this.status = Status.READY;
         this.reg = new int[10];
     }
 
+    //retorna a lista de paginas de um processo
     public ArrayList<Integer> getAllocatedPages() {
         return this.allocatedPages;
     }
@@ -42,12 +32,8 @@ public class PCB {
         return this.id;
     }
 
-    //retorna a lista de paginas de um processo
-    public ArrayList<Integer> getLista(){
-        return this.allocatedPages;
-    }
 }
 
-enum StatusPCB {
-    PRONTO, BLOQUEADO, EXECUTANDO
+enum Status {
+    READY, BLOCKED, RUNNING
 }
